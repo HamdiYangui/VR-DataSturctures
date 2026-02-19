19b760b550cd16812caffd6436b2980b826b172b
HamdiYangui <hamdiyangui97@gmail.com>
2026-02-20 00:09:09 +0100

    Initial commit

--- Assets/scripts/PaddlePickup.cs
+++ Assets/scripts/PaddlePickup.cs
@@ -4,6 +4,8 @@ public class PaddlePickup : MonoBehaviour
 {
     public GameObject boatWithoutPaddles;
     public GameObject boatWithPaddles;
+        public GameObject boatWithoutPaddles2;
+    public GameObject boatWithPaddles2;
     public float interactDistance = 3f;
 
     Transform player;
@@ -28,19 +30,27 @@ public class PaddlePickup : MonoBehaviour
     {
         // Disable old boat
         boatWithoutPaddles.SetActive(false);
+        boatWithoutPaddles2.SetActive(false);
 
         // Enable new boat
         boatWithPaddles.SetActive(true);
+         boatWithPaddles2.SetActive(true);
+
 
         // Match position & rotation
         boatWithPaddles.transform.position = boatWithoutPaddles.transform.position;
         boatWithPaddles.transform.rotation = boatWithoutPaddles.transform.rotation;
 
+         boatWithPaddles2.transform.position = boatWithoutPaddles2.transform.position;
+        boatWithPaddles2.transform.rotation = boatWithoutPaddles2.transform.rotation;
+
         // Enable movement
         BoatController controller = boatWithPaddles.GetComponent<BoatController>();
-        if (controller != null)
+        BoatController controller2 = boatWithPaddles2.GetComponent<BoatController>();
+        if (controller != null || controller2!=null)
         {
             controller.EnableMovement();
+            controller2.EnableMovement();
         }
 
         // Remove paddles from world
